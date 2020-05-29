850f7d379f37db7c62d2cf9a389812d90dfbb66f
goodtobebad <49612579+goodtobebad@users.noreply.github.com>
2020-05-29 22:47:22 +0200

    Update database.php

--- citation/application/config/database.php
+++ citation/application/config/database.php
@@ -4,6 +4,6 @@
  * Database configuration settings used by PDO.
  */
 
-$config['dsn']      = 'mysql:host=localhost;dbname=sev-03_jaha_projet;charset=UTF8';
-$config['password'] = '605621a2MTI5MmQyOGZlM2UzMjAxOTJhZThiMDg1ce686742';
-$config['user']     = 'jaha';
+$config['dsn']      = 'mysql:host=localhost;dbname=*******;charset=UTF8';
+$config['password'] = '******';
+$config['user']     = '******';
