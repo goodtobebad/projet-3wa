14b3d55b9295f4a04119dc39bf52fc785b53f114
goodtobebad <49612579+goodtobebad@users.noreply.github.com>
2020-06-27 20:17:07 +0200

    Update main.js

--- citation/application/www/js/main.js
+++ citation/application/www/js/main.js
@@ -15,28 +15,53 @@
 
 
 
-if(window.location.href.indexOf('/') !== -1) {
+if(window.location.href.indexOf('/') !== -1 && window.location.href.indexOf('/favorite') == -1 ) {
 
-
-    function submitForm(e){
-        event.preventDefault();
-        let id = this.dataset.id;
-        let form = '#fav_form'+ id;
-        $(form).submit();
+    function sendToBackend(){
+        let id = $(this).data('id');
+        $.ajax({
+            url: getRequestUrl()+'/favorite/addtofavorite',
+            type: "POST",
+            data: 'citationId='+id
+        });
+        $(this).toggleClass('red');
+        return false;
     }
     
-    $(document).on('click', '.heart', submitForm);
+    
+    $(document).on('click', '.heart', sendToBackend);
+}
 
+if(window.location.href.indexOf('/favorite') !== -1) {
+
+    function sendToBackend(){
+        let id = $(this).data('id');
+        $.ajax({
+            url: getRequestUrl()+'/favorite/addtofavorite',
+            type: "POST",
+            data: 'citationId='+id
+        });
+        $(this).toggleClass('red');
+        
+        if($(this).css('color') !== 'rgb(255, 0, 0)'){
+            $(this).parent().parent().parent().remove();
+        }
+        return false;
+    }
+    
+    
+    $(document).on('click', '.heart', sendToBackend);
+    
 }
 
 
- if(window.location.href.indexOf('/comment') !== -1) {
-//      pour pas que le formulaire s'envoie à chaque refresh
-     
+if(window.location.href.indexOf('/comment') !== -1) {
     if ( window.history.replaceState ) {
       window.history.replaceState( null, null, window.location.href );
-    } 
- }
+    }
+    
+}
+
 
 if(window.location.href.indexOf('/game') !== -1) {
     $('.ennemie_img').css('cursor', 'url('+getWwwUrl()+'/images/cursor/spo15.cur), auto');
